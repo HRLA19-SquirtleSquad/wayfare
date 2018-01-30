f7d3c2408ade80877f8f87f76cc7535dfd5a132e
Brian Hong <ebrianhong@gmail.com>
2018-01-29 23:31:22 -0800

    [Fix] - tidy up the code

--- rest-server/src/components/users/usersQuery.js
+++ rest-server/src/components/users/usersQuery.js
@@ -8,9 +8,11 @@ export const getUserQuery = async (body) => {
   try {
     // const queryString = getUserHelper(body.userId);
     // const data = await db.queryAsync(queryString);
+    
     let data = await function() {
       return 'test'
     }
+    
     console.log('getUserQuery - successfully fetched user data', data)
     return data
   } catch(err) {
--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
-// const express = require('express');
 import usersRouter from '../components/users/usersRouter'
 // import listingsRouter from '../components/listings/listingsRouter'
 
-// router.use('/users', usersRouter);
-// router.use('/listings', listingsRouter);
 const router = express.Router();
 
 router.use('/users', usersRouter);
+// router.use('/listings', listingsRouter);
 
 export default router;
 

6e6829530ddad8e1dbc623c6c0e6bf86881c3353
Eric Carmona <ericscarmona@yahoo.com>
2018-01-29 19:52:28 -0800

    [server] working on connecting DB

---
+++ socket-server/src/clientEvents.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import {
+  serverInitialState,
+  serverChanged,
+  serverLeave,
+  serverMessage,
+  serverInitalState
+} from './serverEvents';
+
+const clientReady = ({ io, client, room}) => {
+  console.log('client ready heard');
+  serverInitalState({ io, client, room}, payload);
+} 
+
+// const clientUpdate = ({ io, client, room }, payload) => {
+//   const { chat, email } = payload;
+//   console.log('client update heard, payload:', payload);
+//   room.set('chat', chat);
+//   room.set('email', email);
+//   serverChanged({ io, client, room});
+// };
+
+const clientDisconnect = ({}) => {
+  console.log('client disconnected');
+  serverLeave({ io, room });
+};
+
+const clientMessage = ({ io, room }, payload) => {
+  console.log('client message heard');
+  serverMessage({ io, room }, payload);
+};
+
+const clientEmitters = {
+  'client.ready': clientReady,
+  'client.update': clientUpdate,
+  'client.disconnect': clientDisconnect,
+  'client.message': clientMessage
+}
+
+export default clientEmitters;
